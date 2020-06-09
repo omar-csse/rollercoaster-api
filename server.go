1111842b8db029a0a2579bbccf302216d65ec7ba
omar <omar.csse@gmail.com>
2020-06-09 20:42:22 +1000

    add post method

--- server.go
+++ server.go
@@ -1,48 +1,85 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
+	"net/http"
+	"sync"
 )
 
+type error struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
 type coaster struct {
-	Name 			string `json:"name"`
-	Manufacturer 	string `json:"manufacturer"`
-	ID 				string `json:"id"`
-	InPark 			string `json:"inPark"`
-	Height 			int `json:"height"`
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer"`
+	ID           string `json:"id"`
+	InPark       string `json:"inPark"`
+	Height       int    `json:"height"`
 }
 
 type coastersHandlers struct {
+	sync.Mutex
 	store map[string]coaster
+}
+
+func jsonERROR(w http.ResponseWriter, error interface{}, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(error)
 }
 
 func newCoasterHandlers() *coastersHandlers {
 	return &coastersHandlers{store: map[string]coaster{
-		"id1": coaster{
-			Name: "Fury 325",
+		"id1": {
+			Name:         "Fury 325",
 			Manufacturer: "B+M",
-			ID: "id1",
-			InPark: "Luna Park",
-			Height: 102,
+			ID:           "id1",
+			InPark:       "Luna Park",
+			Height:       102,
 		},
 	}}
 }
 
-func (h *coastersHandlers) get(w http.ResponseWriter, r *http.Request) {
-	coasters := make([]coaster, len(h.store))
+func (h *coastersHandlers) coasters(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.get(w, r)
+		return
+	case "POST":
+		h.post(w, r)
+		return
+	default:
+		jsonERROR(w, error{true, "method not allowed"}, http.StatusMethodNotAllowed)
+		return
+	}
+}
 
-	for i, coaster := range coasters {
-		coasters[i] = coaster
+func (h *coastersHandlers) get(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(h.store)
+}
+
+func (h *coastersHandlers) post(w http.ResponseWriter, r *http.Request) {
+
+	var coaster coaster
+
+	err := json.NewDecoder(r.Body).Decode(&coaster)
+	if err != nil {
+		jsonERROR(w, error{true, "Invalid coaster body"}, http.StatusBadRequest)
+		return
 	}
 
-	json.NewEncoder(w).Encode(coasters)
+	h.Lock()
+	h.store[coaster.ID] = coaster
+	defer h.Unlock()
 }
 
 func main() {
 
 	coasterHandlers := newCoasterHandlers()
 
-	http.HandleFunc("/coasters", coasterHandlers.get)
+	http.HandleFunc("/coasters", coasterHandlers.coasters)
 	http.ListenAndServe(":9000", nil)
 }
